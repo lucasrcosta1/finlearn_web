6c92264fc15dd26199c90b2a95c1258a686a03d0
Lucas Costa <lucasrcosta1@gmail.com>
2023-04-20 23:57:34 -0300

    sidebar changed

---
+++ src/app/models/Tab.model.ts
@@ -0,0 +1,14 @@
+export class Tab {
+  icon: string;
+  route: string;
+
+  /**
+   * Constructor
+   * @param icon
+   * @param route
+   */
+  constructor (icon: string, route: string) {
+    this.icon = icon;
+    this.route = route;
+  }
+}
