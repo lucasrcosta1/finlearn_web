bf1436bef4a3463f29f69c2259f7b1db6d68183b
Gabriel Amaral <gcbsamaral.dev@gmail.com>
2023-06-17 17:13:08 -0300

    home_page_first_delivery

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,10 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  public which_module: string;
 
-  constructor () {
-    this.which_module = "Último"
-  }
+export class HomeComponent {
+
 }
