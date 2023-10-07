2e966cec45fa83d07e0f2595485d39737c8e62c7
Lucas Costa <lucasrcosta1@gmail.com>
2023-10-07 15:14:39 -0300

    login logic (without api) created

--- src/app/components/authentication/login/login-bigger-screen/login-bigger-screen.component.ts
+++ src/app/components/authentication/login/login-bigger-screen/login-bigger-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login-bigger-screen',
@@ -14,12 +16,85 @@ export class LoginBiggerScreenComponent {
   @Input()
   tabTitle: string | null = null;
 
+  loginForm: FormGroup;
+
+  constructor (
+    private _formBuilder: FormBuilder,
+    private _loginService: LoginService,
+  ) {
+
+    this.loginForm = this._createLoginForm(this._formBuilder);
+
+  }
+
+
   /**
    * Emit event and enable register page.
    */
   enableRegisterPage (): void {
 
     this.triggerRegistry.emit(true);
+
+  }
+
+  /**
+   * Submit form data.
+   */
+  submit (): void {
+
+    if (this._checkFieldsCorrectlyFilled()) {
+      
+      const username = this.loginForm.value.username, password = this.loginForm.value.password;
+      this._loginService.login(username, password);
+
+    }
+
+  }
+
+  /**
+   * Create login form.
+   * @param formBuilder 
+   * @returns 
+   */
+  private _createLoginForm (formBuilder: FormBuilder): FormGroup {
+
+    return formBuilder.group({
+
+      username: [null, Validators.email],
+      password: [null, Validators.minLength(6)],
+
+    });
+
+  }
+
+  private _checkFieldsCorrectlyFilled (): boolean{
+
+    let isUsernameFieldOk = false, isPasswordFieldOk = false;
+    if (this._loginService.usernameMatchPattern(this.loginForm.value.username)){
+      
+      this._loginService.removeFieldError("loginBiggerScreenUsername");
+      this._loginService.hideErrorMessage("loginBiggerScreenUsername"+"-error");
+      isUsernameFieldOk = true;
+
+    } else {
+
+      this._loginService.markFieldError("loginBiggerScreenUsername");
+      this._loginService.showErrorMessage("loginBiggerScreenUsername"+"-error");
+
+    }
+    if (this._loginService.passwordMatchPattern(this.loginForm.value.password)) {
+
+      this._loginService.removeFieldError("loginBiggerScreenPasswordDiv");
+      this._loginService.hideErrorMessage("loginBiggerScreenPasswordDiv"+"-error");
+      isPasswordFieldOk = true;
+
+    } else {
+
+      this._loginService.markFieldError("loginBiggerScreenPasswordDiv");
+      this._loginService.showErrorMessage("loginBiggerScreenPasswordDiv"+"-error");
+
+    }
+    return isUsernameFieldOk && isPasswordFieldOk;
 
   }
 
--- src/app/components/authentication/login/login.service.ts
+++ src/app/components/authentication/login/login.service.ts
@@ -1,9 +1,145 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/service/snackbar/snackbar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _snackBarService: SnackbarService,
+  ) { }
+
+  /**
+   * Handle login process based on the credentials passed as parameter.
+   * @param username 
+   * @param password 
+   */
+  login (username: string, password: string): void {
+
+    const route   = '/auth/login';
+    const body    = `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    localStorage.setItem('email', username);
+    localStorage.setItem('username', username);
+
+    // let response = await this._api.post(route, body, null, headers);
+    // if (response.getSuccess()) {
+    //   this.spinner.emit(false);
+    //   localStorage.setItem('id', response.getResponse()?.user.id);
+    //   localStorage.setItem('email', response.getResponse()?.user.email);
+    //   localStorage.setItem('token', response.getResponse()?.access_token);
+    //   localStorage.setItem('username', response.getResponse()?.user.name);
+    //   this._loginService.setUser(new User(response.getResponse().user));
+
+    //   this.spinner.emit(true);
+    //   this._snackBarService.openSnackBar(2, `Bem vindo ${response.getResponse()?.user.name}!`);
+    // } else {
+    //   this._snackBarService.openSnackBar(3,response.getResponse().message);
+    // }
+    this._snackBarService.openSnackBar(2, `Seja bem vindo, ${username}!`);
+    this._router.navigate(['/']);
+
+  }
+
+
+  /**
+   * Username match login pattern.
+   * @param username 
+   * @returns 
+   */
+  usernameMatchPattern (username: string | null): boolean {
+
+    if (username && username != "" && this.usernameIsAnEmail(username)) return true;
+    return false;
+
+  }
+
+  /**
+   * Password match login pattern.
+   * @param password 
+   * @returns 
+   */
+  passwordMatchPattern (password: string | null): boolean {
+
+    return (password && password.length > 6) ? true : false ;
+
+  }
+
+  /**
+   * Check whether an username is an email or not.
+   * @param username 
+   * @returns 
+   */
+  usernameIsAnEmail (username: string): boolean {
+
+    return username.includes("@") && (username.includes(".com") || username.includes(".org"));
+
+  }
+
+  /**
+   * Set field in error by given id.
+   * @param fieldName
+   */
+  markFieldError (fieldName: string): void {
+
+    const fieldDiv = document.getElementById(fieldName);
+    if (fieldDiv) {
+
+      fieldDiv.style.border = "1px solid red";
+
+    }
+
+  }
+
+  /**
+   * Set field out from error by a given id.
+   * @param fieldName
+   */
+  removeFieldError (fieldName: string): void {
+
+    const fieldDiv = document.getElementById(fieldName);
+    if (fieldDiv) {
+
+      fieldDiv.style.border = "1px solid black";
+
+    }
+
+  }
+
+  /**
+   * Show error message.
+   * @param spanIdName 
+   */
+  showErrorMessage (spanIdName: string): void {
+
+    const span = document.getElementById(spanIdName);
+    if (span) {
+
+      span.style.display = "block";
+
+    }
+
+  }
+
+  /**
+   * Hide error message.
+   * @param spanIdName 
+   */
+  hideErrorMessage (spanIdName: string): void {
+
+    const span = document.getElementById(spanIdName);
+    if (span) {
+
+      span.style.display = "none";
+
+    }
+
+  }
+
+  
 }
