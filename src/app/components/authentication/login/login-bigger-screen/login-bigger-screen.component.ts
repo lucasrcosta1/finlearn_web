47dcdac798593259acb508735511e83850cd6e61
Lucas Costa <lucasrcosta1@gmail.com>
2023-10-07 13:59:42 -0300

    bigger screen page created for both login and register (missing authentication logic)

--- src/app/components/authentication/login/login-bigger-screen/login-bigger-screen.component.ts
+++ src/app/components/authentication/login/login-bigger-screen/login-bigger-screen.component.ts
@@ -1,14 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login-bigger-screen',
   templateUrl: './login-bigger-screen.component.html',
-  styleUrls: ['./login-bigger-screen.component.css']
+  styleUrls: ['./login-bigger-screen.component.css', "../../../../../styles.css"]
 })
 export class LoginBiggerScreenComponent {
 
   @Output() 
   triggerRegistry = new EventEmitter<boolean>();
+  @Input()
+  imagePath: string | null = null;
+  @Input()
+  tabTitle: string | null = null;
 
   /**
    * Emit event and enable register page.
--- src/app/components/authentication/register/register-bigger-screen/register-bigger-screen.component.ts
+++ src/app/components/authentication/register/register-bigger-screen/register-bigger-screen.component.ts
@@ -1,14 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register-bigger-screen',
   templateUrl: './register-bigger-screen.component.html',
-  styleUrls: ['./register-bigger-screen.component.css']
+  styleUrls: ['./register-bigger-screen.component.css', "../../../../../styles.css"]
 })
 export class RegisterBiggerScreenComponent {
 
   @Output() 
   triggerLogin = new EventEmitter<boolean>();
+  @Input()
+  imagePath: string | null = null;
+  @Input()
+  tabTitle: string | null = null;
 
   /**
    * Emit event and enable login page.
