05ee4d0f3201e094a6c40f92081aa57ea2bbe94b
Lucas Costa <lucasrcosta1@gmail.com>
2023-10-20 18:42:51 -0300

    first page of practice created

--- src/app/components/secondary-practice/secondary-practice.component.spec.ts
+++ src/app/components/secondary-practice/secondary-practice.component.spec.ts
@@ -1,25 +1,55 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { SecondaryPracticeComponent } from './secondary-practice.component';
-// import { HttpClientModule } from '@angular/common/http';
+import { SecondaryPracticeComponent } from './secondary-practice.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from 'src/app/service/api/api.service';
+import { InvestType } from 'src/app/models/practice/InvestType.model';
 
-// describe('SecondaryPracticeComponent', () => {
-//   let component: SecondaryPracticeComponent;
-//   let fixture: ComponentFixture<SecondaryPracticeComponent>;
+describe('SecondaryPracticeComponent', () => {
+  let component: SecondaryPracticeComponent;
+  let fixture: ComponentFixture<SecondaryPracticeComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ SecondaryPracticeComponent ],
-//       imports: [HttpClientModule], 
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SecondaryPracticeComponent ],
+      imports: [HttpClientModule],
+      providers: [ApiService],
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(SecondaryPracticeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(SecondaryPracticeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  
+  it('should fetch api to get investments registered', async () => {
+    const apiService = TestBed.inject(ApiService);
+
+    const mockDataResponse = new Map<number, InvestType>([
+      [0, {id: 0, name: "Tesouro Selic", redemptionPeriod: "1 ano", interestRate: 0.1275}],
+      [1, {id: 1, name: "Tesouro IPCA", redemptionPeriod: "3 anos", interestRate: 0.465}],
+      [2, {id: 2, name: "Tesouro Prefixado", redemptionPeriod: "3 anos", interestRate: 0.3348}],
+      [3, {id: 3, name: "CDB - liquidez diária", redemptionPeriod: "1 ano", interestRate: 0.15}],
+      [4, {id: 4, name: "Letra de Crédito Imobiliário - liquidez diária", redemptionPeriod: "1 ano", interestRate: 13.65}],
+      [5, {id: 5, name: "Letra de Crédito do Agronegócio - liquidez diária", redemptionPeriod: "1 ano", interestRate: 13.65}],
+      [6, {id: 6, name: "Certificado de Recebíveis Imobiliários - liquidez diária", redemptionPeriod: "1 ano", interestRate: 0.12}],
+      [7, {id: 7, name: "Certificado de Recebíveis do Agronegócio - liquidez diária", redemptionPeriod: "1 ano", interestRate: 0.13}],
+      [8, {id: 8, name: "Poupança - liquidez diária", redemptionPeriod: "1 ano", interestRate: 0.45}],
+    ]);
+
+    spyOn(apiService, 'getInvestmentTypes').and.returnValue(Promise.resolve(mockDataResponse));
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    const fieldInvestTypeArray = Array.from(component.fieldInvestType.values());
+    const expectedArray = Array.from(mockDataResponse.values());
+    expect(fieldInvestTypeArray).toEqual(jasmine.arrayContaining(expectedArray));
+
+  });
+});
--- src/app/components/secondary-practice/secondary-practice.component.ts
+++ src/app/components/secondary-practice/secondary-practice.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { InvestType } from 'src/app/models/practice/InvestType.model';
+import { ApiService } from 'src/app/service/api/api.service';
 import { PratiqueService } from 'src/app/service/pratique/pratique.service';
 
 @Component({
@@ -9,9 +11,19 @@ import { PratiqueService } from 'src/app/service/pratique/pratique.service';
 })
 export class SecondaryPracticeComponent {
 
+  image = "/assets/images/pratique/bro.svg";
+  fieldInvestType = new Map<number, InvestType>();
+
   constructor (
     private _pratiqueService: PratiqueService,
+    private _apiService: ApiService,
   ) {}
+
+  async ngOnInit (): Promise<void> {
+
+    this.fieldInvestType =  await this._apiService.getInvestmentTypes();
+
+  }
 
   /**
    * Send user to the requested page.
