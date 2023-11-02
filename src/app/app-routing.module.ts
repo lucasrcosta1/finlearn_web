e525507a636085f236c08dbd1d617aebbbe33dcf
Lucas Costa <lucasrcosta1@gmail.com>
2023-11-02 12:24:16 -0300

    practice component changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ import { CommunityComponent } from './components/community/community.component';
 import { LearnComponent } from './components/learn/learn.component';
 import { RouteTestComponent } from './components/route-test/route-test.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { PracticeRoutingModule } from './components/secondary-practice/practice-routing.module';
-import { SecondaryPracticeComponent } from './components/secondary-practice/secondary-practice.component';
+import { PracticeRoutingModule } from './components/practice/practice-routing.module';
+import { PracticeComponent } from './components/practice/practice.component';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 
 
@@ -23,7 +23,7 @@ const routes: Routes = [
   {path: 'learn', component: LearnComponent, canActivate: [AuthGuard]},
   {path: 'support', component: SuporteComponent, canActivate: [AuthGuard]},
   {path: 'community', component: CommunityComponent, canActivate: [AuthGuard]},
-  {path: 'practice', component: SecondaryPracticeComponent, canActivate: [AuthGuard]},
+  {path: 'practice', component: PracticeComponent, canActivate: [AuthGuard]},
   {path: 'route_test', component: RouteTestComponent, canActivate: [AuthGuard]},
   {path: 'auth/login', component: AuthenticationComponent},
   {path: '**', redirectTo: 'page_not_found'}, // create a not found page.
