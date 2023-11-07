81b92ffe8e5946c848342c0fd8d211f7cbbd0a08
Lucas Costa <lucasrcosta1@gmail.com>
2023-11-06 21:03:43 -0300

    frequent question updated

--- src/app/models/contact/FrequentQuestion.model.ts
+++ src/app/models/contact/FrequentQuestion.model.ts
@@ -3,7 +3,6 @@ export class FrequentQuestion {
     id: number | null;
     name: string | null;
     subject: string | null; 
-    description: string | null; 
     answer: string | null; 
     expanded: boolean; 
 
@@ -12,7 +11,6 @@ export class FrequentQuestion {
         this.id = frequentQuestion?.id || null;
         this.name = frequentQuestion?.name || null;
         this.subject = frequentQuestion?.subject || null;
-        this.description = frequentQuestion?.description || null;
         this.answer = frequentQuestion?.answer || null;
         this.expanded = frequentQuestion?.expanded || false;
 
