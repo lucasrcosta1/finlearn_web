ff386934252283ebfb0a8880543550dea736cc09
Lucas Costa <lucasrcosta1@gmail.com>
2023-11-13 16:34:43 -0300

    test import error

--- src/app/components/authentication/guards/auth-guard.service.spec.ts
+++ src/app/components/authentication/guards/auth-guard.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthGuard } from './auth-guard.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 describe('AuthGuard', () => {
@@ -8,7 +9,7 @@ describe('AuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, MatSnackBarModule]
     });
     service = TestBed.inject(AuthGuard);
   });
--- src/app/components/authentication/login/login.service.spec.ts
+++ src/app/components/authentication/login/login.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginService } from './login.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginService', () => {
   let service: LoginService;
